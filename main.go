0c3900c085c55ff726b47259d421e292abebcc1c
Chang-Wen <eric81510@gmail.com>
2024-03-10 00:09:56 +0800

    v2

--- main.go
+++ main.go
@@ -15,21 +15,19 @@ func main() {
 // )
 
 // func main() {
-// 	// 指定要爬取的网页地址
+
 // 	url := "https://chang-wen-chen.github.io/2024/03/sw_gcloud_storage"
 
-// 	// 发送 HTTP GET 请求
 // 	response, err := http.Get(url)
 // 	if err != nil {
-// 		fmt.Println("HTTP GET 请求失败:", err)
+
 // 		return
 // 	}
 // 	defer response.Body.Close()
 
-// 	// 读取响应内容
 // 	body, err := ioutil.ReadAll(response.Body)
 // 	if err != nil {
-// 		fmt.Println("读取响应内容失败:", err)
+
 // 		return
 // 	}
 
